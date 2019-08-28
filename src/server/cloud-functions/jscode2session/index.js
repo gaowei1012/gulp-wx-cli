f4d99d465be22a84cabda4c25ddba3ccbe3fad75
gmw-zjw <mingwei2222@126.com>
2019-08-28 11:05:45 +0800

    jscde2seesion

---
+++ src/server/cloud-functions/jscode2session/index.js
@@ -0,0 +1,47 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-27 18:53:09
+ * @LastEditTime: 2019-08-28 11:00:36
+ * @LastEditors: Please set LastEditors
+ */
+
+const WX_API = 'https://api.weixin.qq.com/sns/jscode2session'
+const request = require('request')
+const querystring = require('querystring')
+
+const $ = require('../../inline/utils')
+
+
+exports.main = async (event) => {
+  // 获取微信登录接口调用
+  const { code } = event;
+  let {id, sk} = $.getWechatAppConfig();
+
+  const data = {
+    appid: id,
+    secret: sk,
+    js_code: code,
+    grant_type: 'authorization_code'
+  };
+
+  let url = `${WX_API}?${querystring.stringify(data)}`;
+  console.log(url)
+
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err || response.statusCode !== 200) {
+        reject(err)
+      } else {
+        try {
+          let res = JSON.parse(body)
+          console.log(res)
+          console.log(res)
+          resolve(res)
+        } catch (e) {
+          reject(e)
+        }
+      }
+    })
+  })
+}
--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@
  * @Description: wx cloud server index 
  * @Author: your name
  * @Date: 2019-08-27 18:50:32
- * @LastEditTime: 2019-08-28 10:34:30
+ * @LastEditTime: 2019-08-28 10:59:15
  * @LastEditors: Please set LastEditors
  */
 const path = require('path');
@@ -12,6 +12,7 @@ const { PORT } = require('../../config.server.json');
 const heAir = require('./cloud-functions/he-air').main;
 const heWeather = require('./cloud-functions/he-weather').main;
 const gecoder = require('./cloud-functions/geocoder').mian;
+const jscode2session = require('./cloud-functions/jscode2session').main;
 
 console.log(heAir)
 
@@ -38,6 +39,13 @@ app.get('/api/he-weather', (req, res, next) => {
 
 app.get('/api/gecoder', (req, res, next) => {
   gecoder(req.query).then(res.json.bind(res)).catch((e) => {
+    console.error(e)
+    next()
+  })
+})
+
+app.get('/api/jscode2session', (req, res, next) => {
+  jscode2session(req.query).then(res.json.bind(res)).catch((e) => {
     console.error(e)
     next()
   })
